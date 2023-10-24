4cbe9de144e363f76ac14f8e4e4d9f4a54f94c96
alexr-Dingus <alex@therobotfuture.com>
2023-10-24 19:53:15 +0000

    Removing the temp password

--- test_agent.py
+++ test_agent.py
@@ -1,9 +1,12 @@
 import torch
+import os
 from transformers.tools import LocalAgent
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import structify
 
-structify.login("alex@structify.ai", "123WestSt")
+username = os.environ.get("STRUCTIFY_USERNAME")
+password = os.environ.get("STRUCTIFY_PASSWORD")
+structify.login(username, password)
 
 checkpoint = "meta-llama/Llama-2-13b-chat-hf"
 model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map="auto", torch_dtype=torch.bfloat16)
