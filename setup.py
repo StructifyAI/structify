72db7f5f290c7c8b493bc7e40635cbb3a6107fad
alexr-Dingus <alex@therobotfuture.com>
2023-11-05 21:54:32 +0000

    Structuring for pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='structify',
+    version='0.1.0',    
+    description='A complete copilot data solution',
+    url='https://github.com/StructifyAI/structify_tool',
+    author='Alex Reichenbach',
+    author_email='alex@structify.ai',
+    license='MIT License',
+    packages=['structify'],
+    install_requires=[],
+    classifiers=[],
+)
