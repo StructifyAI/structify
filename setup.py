7ee591a9df138197db5cdf61ec9b96b958ffc6a6
Alex Reichenbach <8087473+Reichenbachian@users.noreply.github.com>
2024-02-21 22:29:31 +0000

    bumpming version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import find_packages, setup  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "structifyai"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = ["urllib3 >= 1.25.3, < 2.1.0", "python-dateutil", "pydantic >= 1.10.5, < 2", "aenum"]
 
